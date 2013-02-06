0380f3f0b94b9574904f92399587e41ede816b26
Jean-Remi Desjardins <jeanremi.desjardins@gmail.com>
2013-02-06 17:54:52 -0500

    detail specification on Worksheet and Parser

--- test/ParserSpec.scala
+++ test/ParserSpec.scala
@@ -7,7 +7,7 @@ import models.AST._
 class ParserSpec extends Specification {
   "Parser" should {
 
-    "parse a simple numerical expression" in {
+    "parse a simple numerical expression such as \"32\"" in {
       val expression = "32"
       Parser.parseSingleValidStatement(expression) must be equalTo ObjectExpression(Number_(32))
     }
@@ -17,13 +17,13 @@ class ParserSpec extends Specification {
       Application(Reference("+", Some(ObjectExpression(Number_(first)))), params)
     }
     
-    "parse a simple addition expression" in {
+    "parse a simple addition expression such as \"7.+(4)\"" in {
       val expression = "7.+(4)"
       val actual = Parser.parseSingleValidStatement(expression)
       actual must be equalTo additionExpression(7, 4)
     }
     
-    "parse an infix operator notation addition expression" in {
+    "parse an infix operator notation addition expression such as \"7 + 4\"" in {
       val expression = "7 + 4"
       val actual = Parser.parseSingleValidStatement(expression)
       actual must be equalTo additionExpression(7, 4)
--- test/WorkSheetSpec.scala
+++ test/WorkSheetSpec.scala
@@ -8,19 +8,19 @@ class WorkSheetSpec extends Specification {
 
   "WorkSheet" should {
 
-    "compute a simple numerical expression" in {
+    "compute a simple numerical expression such as \"7.+(4)\"" in {
       val code = "7.+(4)"
       
       WorkSheet.computeResults(code) must be equalTo List("11")
     }
     
-    "handle an incorrect expression" in {
+    "handle an incorrect expression such as \"7.\" by printing \"invalid expression\"" in {
       val code = "7."
       
       WorkSheet.computeResults(code) must be equalTo List("invalid expression")
     }
     
-    "handle an invalid reference" in {
+    "handle an invalid reference by printing \"invalid reference: a\" for a reference to an unknown a" in {
       val code = "a"
       
       WorkSheet.computeResults(code) must be equalTo List("invalid reference: a")
