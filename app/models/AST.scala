2f3ae64ae388dc6e10ba1e61dd71f98da7caf65a
Jean-Remi Desjardins <jeanremi.desjardins@gmail.com>
2013-02-06 17:40:41 -0500

    Refactor AST to support equation resolution and unresolved expressions

--- app/models/AST.scala
+++ app/models/AST.scala
@@ -4,40 +4,70 @@ trait Statement
 
 case class Assignement(name: String, value: Expression) extends Statement
 
-trait Expression extends Statement {
-  def evaluate(assignements: Map[String, Object_] = Map()): Object_
+trait Value
+
+trait Expression extends Statement with Value {
+  def evaluate(assignements: Map[String, Value] = Map()): Value
 }
 
 case class UncompiledExpression(content: String) extends Expression {
-  def evaluate(assignements: Map[String, Object_]) =
-    Parser.parseSingleStatement(content).asInstanceOf[Expression].evaluate(assignements)
+  def evaluate(assignements: Map[String, Value]) =
+    Parser.parseSingleValidStatement(content).asInstanceOf[Expression].evaluate(assignements)
 }
 
 case class ObjectExpression(object_ : Object_) extends Expression {
-  def evaluate(assignements: Map[String, Object_]) = object_
+  def evaluate(assignements: Map[String, Value]): Object_ = object_
 }
 
 case class Reference(name: String, parent: Option[Expression] = None) extends Expression {
-  def evaluate(assignements: Map[String, Object_]) = {
+  def evaluate(assignements: Map[String, Value]) = {
+    println("Reference")
     parent match {
-      case Some(parent) => parent.evaluate(assignements).members(name)
-      case None => try {assignements(name)} catch { case e: NoSuchElementException => throw new InvalidReferenceException(name) }
+      case Some(parent) => parent.evaluate(assignements) match {
+	case obj: Object_     => println("1");obj.members.getOrElse(name, this)
+	case expr: Expression => println("2");this
+      }
+      case None => println("3");assignements.applyOrElse(name, (key: String) => this)
     }
   }
 }
 
-case class InvalidReferenceException(name: String) extends Exception
-
-case class Application(reference: Expression, arguments: List[Expression]) extends Expression {
-  def evaluate(assignements: Map[String, Object_]) = {
-    reference.evaluate(assignements) match {
-      case function: Function => function.apply_(arguments.map(_.evaluate(assignements)))
-      case object_ => object_.members("()").asInstanceOf[Function].apply_(arguments.map(_.evaluate(assignements)))
-    }
+object ExpressionImplicits {
+  implicit def Object2Expr(value: Object_) = ObjectExpression(value)
+  implicit def Value2Expr(value: Value): Expression = value match {
+    case obj: Object_ => obj
+    case expr: Expression => expr
+  }
+  implicit def ValueList2ExprList(value: List[Value]): List[Expression] = {
+    println(value)
+    for (element <- value) yield Value2Expr(element)
   }
 }
 
-trait Object_ {
+case class Application(expr: Expression, arguments: List[Expression]) extends Expression {
+  def evaluate(assignements: Map[String, Value]) = {
+    println("Application")
+    println(expr)
+    val onWhichToApply = expr.evaluate(assignements)
+    println("Application1")
+    val resolvedArguments = arguments.map(_.evaluate(assignements))
+    val hasUnresolvedArguments = resolvedArguments.exists(_.isInstanceOf[Expression])
+    if (hasUnresolvedArguments)
+      Application(ExpressionImplicits.Value2Expr(onWhichToApply), ExpressionImplicits.ValueList2ExprList(resolvedArguments))
+    else {
+      println("Application2")
+      val argumentsAsObject = resolvedArguments.map(_.asInstanceOf[Object_])
+      onWhichToApply match {
+	case expr: Expression   => println(resolvedArguments.length);Application(expr, ExpressionImplicits.ValueList2ExprList(resolvedArguments))
+	case function: Function => println("Application4");function.apply_(argumentsAsObject)
+	case obj: Object_ => println("Application5");obj.members("()").asInstanceOf[Function].apply_(argumentsAsObject)
+      }
+    }
+  }
+}
+    
+
+trait Object_ extends Value {
   def members: Map[String, Object_]
 }
 
@@ -67,30 +97,10 @@ case class Number_(value: Double) extends Object_
   override def toString = if (value.isValidInt) value.toInt.toString else value.toString
 }
 
-object Parser {
-  def parse(content: String): List[Statement] = {
-    val lines = content.split('\n')
-    for (line <- lines if line != "") yield parseSingleStatement(line) match {
-      case Expression.Success(stat, _) => stat
-    }
-  }.toList
-      
-  def parseSingleStatement(content: String) = {
-    if (content.contains('\n')) throw new UnexpectedEndOfLineError
-    Expression.parseAll(Expression.statement, content)
-  }
-  
-  def parseSingleValidStatement(content: String) = {
-    parseSingleStatement(content) match {
-      case Expression.Success(stat, _) => stat
-    }
-  }
-}
-
 class UnexpectedEndOfLineError extends Exception
 import scala.util.parsing.combinator._
 
-object Expression extends RegexParsers with PackratParsers {
+object StatementParser extends RegexParsers with PackratParsers {
 
   val integer = """[1-9][0-9]*"""r
   
@@ -124,24 +134,43 @@ object Expression extends RegexParsers with PackratParsers {
   }
 }
 
+object Parser {
+  def parse(content: String): List[Statement] = {
+    val lines = content.split('\n')
+    for (line <- lines if line != "") yield parseSingleStatement(line) match {
+      case StatementParser.Success(stat, _) => stat
+    }
+  }.toList
+  
+  def parseSingleStatement(content: String) = {
+    if (content.contains('\n')) throw new UnexpectedEndOfLineError
+    StatementParser.parseAll(StatementParser.statement, content)
+  }
+  
+  def parseSingleValidStatement(content: String) = {
+    parseSingleStatement(content) match {
+      case StatementParser.Success(stat, _) => stat
+    }
+  }
+}
+
 object WorkSheet {
   def computeResults(code: String): List[String] = {
   
-    var assignements: Map[String, Object_] = Map()
+    var assignements: Map[String, Value] = Map()
   
     code.split('\n').toList.map{ line =>
       if (line == "") ""
-      else try {
-	Parser.parseSingleStatement(line) match {
-	  case Expression.Success(ass: Assignement, _) => {
-	    assignements = assignements + (ass.name -> ass.value.evaluate(assignements))
-	    ass.name + " = " + ass.value.evaluate(assignements).toString
-	  }
-	  case Expression.Success(exp: Expression, _) => exp.evaluate(assignements).toString
-	  case _ => "invalid expression"
+      else Parser.parseSingleStatement(line) match {
+	case StatementParser.Success(ass: Assignement, _) => {
+	  assignements = assignements + (ass.name -> ass.value.evaluate(assignements))
+	  ass.name + " = " + ass.value.evaluate(assignements).toString
 	}
-      } catch {
-	case InvalidReferenceException(name) => s"invalid reference: $name is not found"
+	case StatementParser.Success(exp: Expression, _) => exp.evaluate(assignements) match {
+	  case ref: Reference => s"invalid reference: ${ref.name}"
+	  case obj: Object_ => obj.toString
+	}
+	case _ => "invalid expression"
       }
     }
   }
