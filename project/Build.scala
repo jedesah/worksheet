d53de0c2953c2f3a47d7242c1c195285dc8ac0f5
Jean-Remi Desjardins <jeanremi.desjardins@gmail.com>
2013-09-19 13:05:54 -0400

    Upgrade form scala 2.10.3-RC1 to 2.10.3-RC2

--- project/Build.scala
+++ project/Build.scala
@@ -8,13 +8,13 @@ object ApplicationBuild extends Build {
   val appVersion      = "0.2-SNAPSHOT"
 
   val appDependencies = Seq(
-    "org.scala-lang" % "scala-compiler" % "2.10.3-RC1"
+    "org.scala-lang" % "scala-compiler" % "2.10.3-RC2"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     testOptions in Test += Tests.Argument("junitxml", "console"),
-    scalaVersion := "2.10.3-RC1"
+    scalaVersion := "2.10.3-RC2"
   )
 
 }
